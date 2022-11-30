4a2feeb28b340a89c3ec7d95d82295e4acb51ec5
vikash <44chansw@gmail.com>
2022-11-30 16:22:54 +0530

    just shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Tweetsstate } from './tweetswithJotai'
+import { Tweetsstate } from './tweets'
 
 function App() {
   const [count, setCount] = useState(0)
